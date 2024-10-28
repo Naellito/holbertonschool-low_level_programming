a2ef36afefc0de4b70d749e37104fe03c93ad507
nael <naelelafia12@gmail.com>
2024-10-28 15:38:14 +0100

    Adding main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef PUTCHAR
+#define PUTCHAR
+
+int	_putchar();
+int	_islower();
+int	_isalpha();
+int	print_sign();
+int	_abs();
+int	print_last_digit();
+void	jack_bauer();
+void	ft_printnum();
+int	add();
+void	times_table();
+void	print_to_98();
+
+#endif
