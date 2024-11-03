6bbd8ee49bba26e96b0193ccb234268f398124e2
nael <naelelafia12@gmail.com>
2024-11-03 14:31:54 +0100

    Write a function that prints the 9 times table, starting with 0.

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_num - Print the number.
+ *
+ * @n: n int
+ */
+
+void    print_num(int n)
+{
+        int     n1;
+        int     n2;
+
+        n1 = n / 10;
+        n2 = n % 10;
+        _putchar(n1 + '0');
+        _putchar(n2 + '0');
+}
+
+/**
+ * times_table - Table of nine
+ *
+ */
+
+void    times_table(void) 
+{
+        int     i;
+        int     j;
+        int     result;
+
+        i = 0;
+        while (i < 10)
+        {
+                j = 0;
+                while (j < 10)
+                {
+                        result = i * j;
+                        if (j > 0 && result < 10)
+                                _putchar(' '); 
+                        if (result > 9)
+                                print_num(result);
+                        else
+                                _putchar(result + '0');
+                        if (j < 9)
+                        {
+                                _putchar(',');
+                                _putchar(' ');  
+                        }
+                        j++;
+                }
+                _putchar('\n'); 
+                i++;
+        }
+        result = 0; // Erreur 1 : La variable result n'est pas appropriée pour l'utilisation dans ce contexte
+}
