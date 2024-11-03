0e1a1c0c89e19da0f25b85e25bc6a1265f824389
nael <naelelafia12@gmail.com>
2024-11-03 14:36:24 +0100

    fix - Incorrect use of character on _putchar on line 49

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -46,7 +46,7 @@ void    times_table(void)
                         if (j < 9)
                         {
                                 _putchar(',');
-                                _putchar(' ');  
+                                _putchar('a');  
                         }
                         j++;
                 }
