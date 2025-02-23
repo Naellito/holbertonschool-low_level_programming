6ba427a7b23ec7adb444434d0c1402f6fc8cd853
Naellito <naelelafia12@gmail.com>
2025-02-23 22:57:16 +0100

    hot fix - main.h adding the void print_alphabet

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,16 +1,17 @@
-#ifndef PUTCHAR
-#define PUTCHAR
+#ifndef MAIN_H
+#define MAIN_H
 
-int	_putchar();
-int	_islower();
-int	_isalpha();
-int	print_sign();
-int	_abs();
-int	print_last_digit();
-void	jack_bauer();
-void	ft_printnum();
-int	add();
-void	times_table();
-void	print_to_98();
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void ft_printnum(void);
+int add(int a, int b);
+void times_table(void);
+void print_to_98(int n);
 
 #endif
