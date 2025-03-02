fb17b1a3fc9027ad178a094c6cdea3454a3fac7b
Naellito <naelelafia12@gmail.com>
2025-03-02 22:55:27 +0100

    Adding all the project complet

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * more_numbers - Function more numbers
+ */
+
+void    more_numbers(void)
+{
+	int     i;
+	int	j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+				_putchar(i + '0');
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
+	}
+}
